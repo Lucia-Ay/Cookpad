b99a10d1226e53c0f1381ded596744d1942f6ed0
Lucia Aydin <0itslucia0@gmail.com>
2023-09-07 20:31:07 +0300

    Update global & home.js

--- src/js/global.js
+++ src/js/global.js
@@ -1,3 +1,7 @@
+'use strict';
 
-
-"use strict";
+window.addEventOnElements = ($elements, eventType, callback) => {
+  for (const $element of $elements) {
+    $element.addEventlistener(eventType, callback);
+  }
+};
