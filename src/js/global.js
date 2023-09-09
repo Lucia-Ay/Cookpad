6af72277ae5c484720a78b1d4e4f741cf1a7c665
Lucia Aydin <0itslucia0@gmail.com>
2023-09-09 14:27:53 +0300

    Update global.js

--- src/js/global.js
+++ src/js/global.js
@@ -1,2 +1,42 @@
 'use strict';
 
+import { fetchData } from './api.js';
+
+export const cardQueries = [
+  ['field', 'uri'],
+  ['field', 'label'],
+  ['field', 'image'],
+  ['field', 'totalTime'],
+];
+
+export const $skeletonCard = `
+    <div class="card skeleton-card">
+        <div class="skeleton card-banner"></div>
+
+        <div class="card-body">
+            <div class="skeleton card-title"></div>
+
+            <div class="skeleton card-text"></div>
+        </div>
+    </div>
+`;
+
+const ROOT = 'https://api.edamam.com/api/recipes/v2';
+
+window.savedRecipe = function (element, recipeId) {
+  const isSaved = window.localStorage.getItem(`CookPad${recipeId}`);
+  ACCESS_POINT = `${ROOT}/${recipeId}`;
+
+  if (!isSaved) {
+    fetchData(cardQueries, function (data) {
+      window.localStorage.setItem(`CookPad${recipeId}`, JSON.stringify(data));
+      element.classList.toggle('saved');
+      element.classList.toggle('removed');
+    });
+    ACCESS_POINT = ROOT;
+  } else {
+    window.localStorage.removeItem(`CookPad${recipeId}`);
+    element.classList.toggle('saved');
+    element.classList.toggle('removed');
+  }
+};
