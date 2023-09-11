c249448d181e54ccdc5439a48a7d97436048dc6b
Lucia Aydin <0itslucia0@gmail.com>
2023-09-11 10:44:10 +0300

    Update saved.js

--- src/js/saved.js
+++ src/js/saved.js
@@ -1,1 +1,95 @@
-"use strict";
+'use strict';
+
+import { getTime } from './module.js';
+
+const savedRecipes = Object.keys(window.localStorage).filter((item) => {
+  return item.startsWith('CookPad');
+});
+
+const $savedRecipeContainer = document.querySelector(
+  '[data-saved-recipe-container]'
+);
+
+$savedRecipeContainer.innerHTML = `
+ <h2 class="headline-small section-title">All Saved Recipes</h2>
+`;
+
+const $gridList = document.createElement('div');
+
+if (savedRecipes.length) {
+  savedRecipes.map((savedRecipe, index) => {
+    const {
+      recipe: { image, label: title, totalTime: cookingTime, uri },
+    } = JSON.parse(window.localStorage.getItem(savedRecipe));
+
+    const recipeId = uri.slice(uri.lastIndexOf('_') + 1);
+    const isSaved = window.localStorage.getItem(`CookPad${recipeId}`);
+
+    const $card = document.createElement('div');
+    $card.classList.add('card');
+    $card.style.animationDelay = `${100 * index}ms`;
+
+    $card.innerHTML = `
+        <figure class="card-media img-holder">
+        <img
+        src="${image}"
+        alt="${title}"
+        class="img-cover"
+        width="195"
+        height="195"
+        loading="lazy"
+        />
+        </figure>
+
+        <div class="card-body">
+        <h3 class="title-small">
+            <a href="./detail.html?recipe=${recipeId}" class="card-link">
+            ${title ?? 'untitled'}
+            </a>
+        </h3>
+
+        <div class="meta-wrapper">
+            <div class="meta-item">
+            <span
+                class="material-symbols-outlined"
+                aria-hidden="true">
+                schedule
+            </span>
+
+            <span class="label-medium">
+            ${getTime(cookingTime).time || '<1'} 
+            ${getTime(cookingTime).timeUnit}
+            </span>
+            </div>
+
+            <button
+            class="icon-btn has-state ${isSaved ? 'saved' : 'removed'}"
+            aria-label="Add to saved recipes" 
+            onclick="savedRecipe(this, '${recipeId}')"
+            >
+            <span
+                class="material-symbols-outlined bookmark-add"
+                aria-hidden="true">
+                bookmark_add
+                </span>
+
+            <span
+                class="material-symbols-outlined bookmark"
+                aria-hidden="true">
+                bookmark
+                </span>
+
+            </button>
+        </div>
+        </div>
+    `;
+
+    $gridList.appendChild($card);
+  });
+} else {
+  $savedRecipeContainer.innerHTML += `
+        <p class="body-large">You haven't saved any recipes yet!</p>
+    `;
+}
+
+$savedRecipeContainer.appendChild($gridList);
